e62b038a0e8a046161847f2e88617e8b7311065f
Pedro Polanco <pedropolanco02@gmail.com>
2021-10-14 11:25:09 -0400

    repository added

--- src/main/java/com/Ecommerce/store/model/Items.java
+++ src/main/java/com/Ecommerce/store/model/Items.java
@@ -5,11 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity //This tells Hibernate to make a table out of this class
 public class Items {
-    @Id
+    @Id //this say that field below is  primary key
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
     private String name;
     private double price;
     private String description;
---
+++ src/main/java/com/Ecommerce/store/repository/ItemsRepository.java
@@ -0,0 +1,7 @@
+package com.Ecommerce.store.repository;
+
+import com.Ecommerce.store.model.Items;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemsRepository extends CrudRepository<Items, Integer> {
+}
