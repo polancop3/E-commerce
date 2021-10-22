07658bec7d29b8ece5358be6bb304946a7cdbbc3
Pedro Polanco <pedropolanco02@gmail.com>
2021-10-22 15:53:23 -0400

    @post request fixed

--- src/main/java/com/Ecommerce/store/controller/ItemsController.java
+++ src/main/java/com/Ecommerce/store/controller/ItemsController.java
@@ -1,28 +1,22 @@
 package com.Ecommerce.store.controller;
-
 import com.Ecommerce.store.model.Items;
 import com.Ecommerce.store.repository.ItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(path = "/demo")
 public class ItemsController {
-
     @Autowired
     private ItemsRepository itemsRepository;
 
-    @PostMapping(path = "/add")
-    public @ResponseBody String addNewItem(@RequestParam String name, @RequestParam String description, @RequestParam double price){
-        Items product = new Items();
-        product.setName(name);
-        product.setDescription(description);
-        product.setPrice(price);
-        return "saved";
+    @PostMapping
+    public Items createItems(@RequestBody Items item){
+        return itemsRepository.save(item);
     }
 
-    @GetMapping(path = "/all")
+    @GetMapping
     public @ResponseBody Iterable<Items> getAllItems(){
         return itemsRepository.findAll();
     }
--- src/main/java/com/Ecommerce/store/model/Items.java
+++ src/main/java/com/Ecommerce/store/model/Items.java
@@ -10,16 +10,10 @@ public class Items {
     @Id //this say that field below is  primary key
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-
     private String name;
     private double price;
-    private String description;
 
-    public Items(/*Integer id, String name, double price, String description*/) {
-//        this.id = id;
-//        this.name = name;
-//        this.price = price;
-//        this.description = description;
+    public Items() {
     }
 
     public Integer getId() {
@@ -46,11 +40,4 @@ public class Items {
         this.price = price;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
